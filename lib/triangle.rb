628404079e77b285b8f15690759e0c5fca76bd2d
James Scalise <jamescscalise@gmail.com>
2019-09-14 21:46:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :sides
+  
+  def initialize(side1, side2, side3)
+    @sides = []
+    @sides << side1
+    @sides << side2
+    @sides << side3
+    @sides = @sides.sort
+  end
+  
+  def kind
+    if ((@sides[0] + @sides[1]) <= @sides[2])
+      begin
+        raise TriangleError
+     rescue TriangleError 
+      end
+    elsif @sides[0] == @sides[1] && @sides[1] == @sides[2]
+      :equilateral
+    elsif @sides[0] == @sides[1] || @sides[1] == @sides[2] || @sides[0] == @sides[2]
+      :isosceles
+    else
+      :scalene
+    end
+  end
+  
+  class TriangleError < StandardError
+    def message
+    "bad"
+   end
+  end
 end
+
+triangle = Triangle.new(0,-4,0)
+puts triangle.sides
