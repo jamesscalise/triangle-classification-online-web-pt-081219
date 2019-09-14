1d9a1f3d1435d12964b49db1e13100d7d5bd0935
James Scalise <jamescscalise@gmail.com>
2019-09-14 21:50:37 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,7 +14,8 @@ class Triangle
     if ((@sides[0] + @sides[1]) <= @sides[2])
       begin
         raise TriangleError
-     rescue TriangleError 
+     rescue TriangleError => error
+      puts error.message
       end
     elsif @sides[0] == @sides[1] && @sides[1] == @sides[2]
       :equilateral
